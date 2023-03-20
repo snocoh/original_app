28dd110a233c9687c00fabdfd7488fda0e49e706
snocoh <s.noco2.lvr@gmail.com>
2023-03-20 22:16:19 +0900

    PAY.JP決済機能

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,40 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+
+  def index
+    @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id || @item.order != nil
+      redirect_to root_path
+    else
+      @order_address = OrderAddress.new
+    end
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_address_params)
+    if @order_address.valid?
+      pay_item
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_address_params
+    params.require(:order_address).permit(:postcode, :prefecture_id, :city, :street, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_address_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+end
