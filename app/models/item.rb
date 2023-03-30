7489841a0de413b326bdf8a83d1ab6db9ac9f845
snocoh <s.noco2.lvr@gmail.com>
2023-03-30 21:19:35 +0900

    トップページview

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
 
   belongs_to :user
-  has_many_attached :images
+  has_many_attached :images, dependent: :destroy
   has_many :item_tags, dependent: :destroy
   has_many :tags, through: :item_tags
   has_one    :order, dependent: :destroy
