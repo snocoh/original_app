c12c866f0c6f8686a4e7b0e4525296dbca4df8dd
snocoh <s.noco2.lvr@gmail.com>
2023-04-13 16:38:59 +0900

    item_tagモデルテスト

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ApplicationRecord
 
-  validates :tag_name, uniqueness: true
+  validates :tag_name, uniqueness: true, presence: true
 
   has_many :item_tags
   has_many :items, through: :item_tags
